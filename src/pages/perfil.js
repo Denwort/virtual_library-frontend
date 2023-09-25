209318e7a289e53ff5bef06106d9c52c82333774
Fabricio <casta2804@gmail.com>
2023-09-24 21:02:02 -0600

    perfilAd

--- src/pages/perfil.js
+++ src/pages/perfil.js
@@ -8,16 +8,24 @@ const Perfil = () => <Layout content={
     <Head>
         <title>Perfil</title>
     </Head>
-    <div>
+    <div id="tituloP">
             <p>Hola, Juan</p>
             <Image src="/divider.png" width={1088} height={1}></Image>
     </div>
     <div id="form_perfil">
-        <div style={{width: '100%', height: '100%', background: '#FEF7FF', borderRadius: 10, border: '1px #6750A4 solid'}} />
-        <div style={{width: '100%', height: '100%', textAlign: 'center', color: '#6750A4', fontSize: 12, fontFamily: 'Roboto', fontWeight: '700', lineHeight: 16, wordWrap: 'break-word'}}>DATOS PERSONALES</div>
-    
-    
-    </div> 
+        <div id="barra_perfil">
+            <div id="barra_texto_selected" className="selected">
+                <p>DATOS PERSONALES</p>
+            </div>
+            <div id="barra_texto_notselected">
+                <p>CUENTA</p>
+            </div>
+            <div id="barra_texto_notselected">
+                <p>PREFERENCIAS</p>
+            </div>
+        </div>
+    </div>
+
 
 </>
 }
