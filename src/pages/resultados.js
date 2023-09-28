33f1c8ed2dd7d4cb08e70b3c41b40d09624e3025
David <denwort@example.com>
2023-09-28 18:01:48 -0500

    pantalla Resultados terminada

--- src/pages/resultados.js
+++ src/pages/resultados.js
@@ -12,24 +12,163 @@ const busqueda = () => <Layout content={
 
     <div class="flex justify-between">
         <h1 class="text-2xl font-semibold mb-4">Búsqueda</h1>
-        <button type="submit" class="bg-purple-primary text-purple-bg px-4 py-2 hover:bg-blue-600 border-2 border-purple-primary rounded-full">Volver a buscar</button>
+        <button type="button" class="bg-purple-primary text-purple-bg px-4 py-2 hover:bg-blue-600 border-2 border-purple-primary rounded-full">Volver a buscar</button>
     </div>
 
     <Image src="/divider.png" width={1088} height={1} class="py-4"></Image>
 
     <div class="flex justify-between">
         <h1 class="text-1xl font-semibold mb-4">Resultados de la busqueda</h1>
-        <button type="submit" class="bg-purple-bg text-purple-primary px-4 py-2 hover:bg-blue-600 border-2 border-purple-primary rounded-full">Ver mis reservas</button>
+        <button type="button" class="bg-purple-bg text-purple-primary px-4 py-2 hover:bg-blue-600 border-2 border-purple-primary rounded-full">Ver mis reservas</button>
     </div>
     
-    <div class="bg-white p-6 rounded-md shadow-md w-12/12 h-full flex">
+    <div class="flex flex-wrap shrink-0 gap-3 bg-white p-6 rounded-md shadow-md w-12/12 h-full justify-center">
 
-        <div class="bg-purple-bg py-4 px-4 w-96 h-96 rounded-lg ">
-            <div>
-                <p class="bg-purple-bg text-purple-primary">PP</p>
+        <div class="w-80 h-96 border-2 border-gray-400 rounded-lg">
+            <div class="h-10 flex justify-between items-center m-4">
+                <div class="w-10 h-10 inline-flex bg-purple-primary text-purple-bg justify-center items-center text-center rounded-full">PP</div>
+                <div class="w-60 pl-2 text-purple-primary text-left items-center align-middle">Clean Code Code Code Code Code Code Code Code Code</div>
+            </div>
+            <div class="flex bg-purple-bg mx-auto justify-center items-center">
+                <Image src="/imagenLibro.png" height={10000} width={10000} alt="libro_imagen" class="h-36 w-auto" ></Image>
+            </div>
+            <div class="py-2 px-4 text-purple-primary">
+                <div class="font-bold">ISBN: 0932633420</div>
+                <div>Autor: 0932633420</div>
+                <div>Editor: Addison-Wesley</div>
+            </div>
+            <div class="h-14 flex justify-center items-center">
+                <button type="button" class="bg-purple-primary text-purple-bg px-4 py-2 hover:bg-blue-600 border-2 border-purple-primary rounded-full">Reservar</button>
             </div>
         </div>
-    
+
+        <div class="w-80 h-96 border-2 border-gray-400 rounded-lg">
+            <div class="h-10 flex justify-between items-center m-4">
+                <div class="w-10 h-10 inline-flex bg-purple-primary text-purple-bg justify-center items-center text-center rounded-full">PP</div>
+                <div class="w-60 pl-2 text-purple-primary text-left items-center align-middle">Clean Code Code Code Code Code Code Code Code Code</div>
+            </div>
+            <div class="flex bg-purple-bg mx-auto justify-center items-center">
+                <Image src="/imagenLibro.png" height={10000} width={10000} alt="libro_imagen" class="h-36 w-auto" ></Image>
+            </div>
+            <div class="py-2 px-4 text-purple-primary">
+                <div class="font-bold">ISBN: 0932633420</div>
+                <div>Autor: 0932633420</div>
+                <div>Editor: Addison-Wesley</div>
+            </div>
+            <div class="h-14 flex justify-center items-center">
+                <button type="button" class="bg-purple-primary text-purple-bg px-4 py-2 hover:bg-blue-600 border-2 border-purple-primary rounded-full">Reservar</button>
+            </div>
+        </div>
+
+        <div class="w-80 h-96 border-2 border-gray-400 rounded-lg">
+            <div class="h-10 flex justify-between items-center m-4">
+                <div class="w-10 h-10 inline-flex bg-purple-primary text-purple-bg justify-center items-center text-center rounded-full">PP</div>
+                <div class="w-60 pl-2 text-purple-primary text-left items-center align-middle">Clean Code Code Code Code Code Code Code Code Code</div>
+            </div>
+            <div class="flex bg-purple-bg mx-auto justify-center items-center">
+                <Image src="/imagenLibro.png" height={10000} width={10000} alt="libro_imagen" class="h-36 w-auto" ></Image>
+            </div>
+            <div class="py-2 px-4 text-purple-primary">
+                <div class="font-bold">ISBN: 0932633420</div>
+                <div>Autor: 0932633420</div>
+                <div>Editor: Addison-Wesley</div>
+            </div>
+            <div class="h-14 flex justify-center items-center">
+                <button type="button" class="bg-purple-primary text-purple-bg px-4 py-2 hover:bg-blue-600 border-2 border-purple-primary rounded-full">Reservar</button>
+            </div>
+        </div>
+
+        <div class="w-80 h-96 border-2 border-gray-400 rounded-lg">
+            <div class="h-10 flex justify-between items-center m-4">
+                <div class="w-10 h-10 inline-flex bg-purple-primary text-purple-bg justify-center items-center text-center rounded-full">PP</div>
+                <div class="w-60 pl-2 text-purple-primary text-left items-center align-middle">Clean Code Code Code Code Code Code Code Code Code</div>
+            </div>
+            <div class="flex bg-purple-bg mx-auto justify-center items-center">
+                <Image src="/imagenLibro.png" height={10000} width={10000} alt="libro_imagen" class="h-36 w-auto" ></Image>
+            </div>
+            <div class="py-2 px-4 text-purple-primary">
+                <div class="font-bold">ISBN: 0932633420</div>
+                <div>Autor: 0932633420</div>
+                <div>Editor: Addison-Wesley</div>
+            </div>
+            <div class="h-14 flex justify-center items-center">
+                <button type="button" class="bg-purple-primary text-purple-bg px-4 py-2 hover:bg-blue-600 border-2 border-purple-primary rounded-full">Reservar</button>
+            </div>
+        </div>
+        
+        <div class="w-80 h-96 border-2 border-gray-400 rounded-lg">
+            <div class="h-10 flex justify-between items-center m-4">
+                <div class="w-10 h-10 inline-flex bg-purple-primary text-purple-bg justify-center items-center text-center rounded-full">PP</div>
+                <div class="w-60 pl-2 text-purple-primary text-left items-center align-middle">Clean Code Code Code Code Code Code Code Code Code</div>
+            </div>
+            <div class="flex bg-purple-bg mx-auto justify-center items-center">
+                <Image src="/imagenLibro.png" height={10000} width={10000} alt="libro_imagen" class="h-36 w-auto" ></Image>
+            </div>
+            <div class="py-2 px-4 text-purple-primary">
+                <div class="font-bold">ISBN: 0932633420</div>
+                <div>Autor: 0932633420</div>
+                <div>Editor: Addison-Wesley</div>
+            </div>
+            <div class="h-14 flex justify-center items-center">
+                <button type="button" class="bg-purple-primary text-purple-bg px-4 py-2 hover:bg-blue-600 border-2 border-purple-primary rounded-full">Reservar</button>
+            </div>
+        </div>
+
+        <div class="w-80 h-96 border-2 border-gray-400 rounded-lg">
+            <div class="h-10 flex justify-between items-center m-4">
+                <div class="w-10 h-10 inline-flex bg-purple-primary text-purple-bg justify-center items-center text-center rounded-full">PP</div>
+                <div class="w-60 pl-2 text-purple-primary text-left items-center align-middle">Clean Code Code Code Code Code Code Code Code Code</div>
+            </div>
+            <div class="flex bg-purple-bg mx-auto justify-center items-center">
+                <Image src="/imagenLibro.png" height={10000} width={10000} alt="libro_imagen" class="h-36 w-auto" ></Image>
+            </div>
+            <div class="py-2 px-4 text-purple-primary">
+                <div class="font-bold">ISBN: 0932633420</div>
+                <div>Autor: 0932633420</div>
+                <div>Editor: Addison-Wesley</div>
+            </div>
+            <div class="h-14 flex justify-center items-center">
+                <button type="button" class="bg-purple-primary text-purple-bg px-4 py-2 hover:bg-blue-600 border-2 border-purple-primary rounded-full">Reservar</button>
+            </div>
+        </div>
+
+        <div class="w-80 h-96 border-2 border-gray-400 rounded-lg">
+            <div class="h-10 flex justify-between items-center m-4">
+                <div class="w-10 h-10 inline-flex bg-purple-primary text-purple-bg justify-center items-center text-center rounded-full">PP</div>
+                <div class="w-60 pl-2 text-purple-primary text-left items-center align-middle">Clean Code Code Code Code Code Code Code Code Code</div>
+            </div>
+            <div class="flex bg-purple-bg mx-auto justify-center items-center">
+                <Image src="/imagenLibro.png" height={10000} width={10000} alt="libro_imagen" class="h-36 w-auto" ></Image>
+            </div>
+            <div class="py-2 px-4 text-purple-primary">
+                <div class="font-bold">ISBN: 0932633420</div>
+                <div>Autor: 0932633420</div>
+                <div>Editor: Addison-Wesley</div>
+            </div>
+            <div class="h-14 flex justify-center items-center">
+                <button type="button" class="bg-purple-primary text-purple-bg px-4 py-2 hover:bg-blue-600 border-2 border-purple-primary rounded-full">Reservar</button>
+            </div>
+        </div>
+
+        <div class="w-80 h-96 border-2 border-gray-400 rounded-lg">
+            <div class="h-10 flex justify-between items-center m-4">
+                <div class="w-10 h-10 inline-flex bg-purple-primary text-purple-bg justify-center items-center text-center rounded-full">PP</div>
+                <div class="w-60 pl-2 text-purple-primary text-left items-center align-middle">Clean Code Code Code Code Code Code Code Code Code</div>
+            </div>
+            <div class="flex bg-purple-bg mx-auto justify-center items-center">
+                <Image src="/imagenLibro.png" height={10000} width={10000} alt="libro_imagen" class="h-36 w-auto" ></Image>
+            </div>
+            <div class="py-2 px-4 text-purple-primary">
+                <div class="font-bold">ISBN: 0932633420</div>
+                <div>Autor: 0932633420</div>
+                <div>Editor: Addison-Wesley</div>
+            </div>
+            <div class="h-14 flex justify-center items-center">
+                <button type="button" class="bg-purple-primary text-purple-bg px-4 py-2 hover:bg-blue-600 border-2 border-purple-primary rounded-full">Reservar</button>
+            </div>
+        </div>
+        
+        
         
     </div>
 
