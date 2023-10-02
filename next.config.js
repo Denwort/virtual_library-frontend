9da14bd7c9c3cc48202c8d168852ad8578d08957
David <denwort@example.com>
2023-10-02 00:04:58 -0500

    jsons agregados

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns : [
+            {
+                protocol : 'https',
+                hostname : 'images.cdn1.buscalibre.com',
+                port: '',
+                pathname : '/**'
+            },
+            {
+                protocol : 'https',
+                hostname : 'images.cdn3.buscalibre.com',
+                port: '',
+                pathname : '/**'
+            },
+            {
+                protocol : 'https',
+                hostname : 'images.cdn2.buscalibre.com',
+                port: '',
+                pathname : '/**'
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
