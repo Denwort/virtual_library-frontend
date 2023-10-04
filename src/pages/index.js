9bdd17df9c8c2c337110286871cdc7e2e357b934
Renzoar500 <renzoar@hotmail.com>
2023-10-04 17:14:33 -0500

    Avance pagina principal

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,81 +23,131 @@ const Index = () => {
                 <p id ="subtitulo">Últimas reservas</p>
                 <br></br>
                 <div class="grid gap-x-14 grid-cols-7 ">
-                    <div class="col-span-1">
-                        <div class="libro">
-                            <div class="grid grid-cols-6 col-span-1">
-                                <div class="col-start-1 col-span-1">
-                                    <div className="circulo">
-                                        <p className="inicial">A</p>
+                <div class="col-span-1">
+                        <Link href="https://www.buscalibre.pe/libro-software-la-superbabosa/9788423687572/p/2858249">
+                            <div class="libro">
+                                <div class="grid grid-cols-6 col-span-1">
+                                    <div class="col-start-1 col-span-1">
+                                        <div class="circulo">
+                                            <p className="inicial">A</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-start-2 col-end-5">
+                                        <div className="contenedorTituloLibro">
+                                            <div class="line-clamp-2">
+                                                <p class= "tituloLibro"><b>"Software la Superbabosa"</b></p>
+                                            </div>
+                                        </div>
+                                        <div className="contenedorInfoLibro">
+                                            <div class="line-clamp-1">
+                                                <p className="infoLibro">18/09/2023 08:00 am</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-start-6 col-span-1">
+                                        <div class="imagenLibro">
+                                            <Image src="https://images.cdn2.buscalibre.com/fit-in/180x180/1f/a6/1fa666e0f93fb0bc63c4c214188f3a46.jpg" width={80} height={101}></Image>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="col-start-2 col-end-5">
-                                    <Link href="">Clean Code</Link>
-                                </div>
-                                <div class="col-start-5 col-span-1">
-
-                                </div>
                             </div>
-                        </div>
+                        </Link>    
                     </div>
                     <div class="col-start-4 col-span-1">
-                        <div class="libro">
-                            <div class="grid grid-cols-6 col-span-1">
-                                <div class="col-start-1 col-span-1">
-                                    <div className="circulo">
-                                        <p className="inicial">A</p>
+                    <Link href="https://www.buscalibre.pe/libro-software-la-superbabosa/9788423687572/p/2858249">
+                            <div class="libro">
+                                <div class="grid grid-cols-6 col-span-1">
+                                    <div class="col-start-1 col-span-1">
+                                        <div class="circulo">
+                                            <p className="inicial">A</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-start-2 col-end-5">
+                                        <div className="contenedorTituloLibro">
+                                            <div class="line-clamp-2">
+                                                <p class= "tituloLibro"><b>"Gestion de proyectos de Software"</b></p>
+                                            </div>
+                                        </div>
+                                        <div className="contenedorInfoLibro">
+                                            <div class="line-clamp-1">
+                                                <p className="infoLibro">18/09/2023 08:00 am</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-start-6 col-span-1">
+                                        <div class="imagenLibro">
+                                            <Image src="https://images.cdn1.buscalibre.com/fit-in/180x180/20/82/2082a4e5a59dcf7e8531feb8e488c30b.jpg" width={80} height={101}></Image>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="col-start-2 col-end-5">
-                                    <Link href="">Introduction to the theory...</Link>
-                                </div>
-                                <div class="col-start-5 col-span-1">
-
-                                </div>
                             </div>
-                        </div> 
+                        </Link>    
                     </div>  
                 </div>
             </div>
             <div class="rectangulo">
-                <p id="subtitulo">Los más pedidos</p>
+                <h2 id="subtitulo">Los más pedidos</h2>
                 <br></br>
                 <div class="grid gap-x-14 grid-cols-7 ">
                     <div class="col-span-1">
-                        <div class="libro">
-                            <div class="grid grid-cols-6 col-span-1">
-                                <div class="col-start-1 col-span-1">
-                                    <div class="circulo">
-                                        <p className="inicial">A</p>
+                        <Link href="https://www.buscalibre.pe/libro-software-la-superbabosa/9788423687572/p/2858249">
+                            <div class="libro">
+                                <div class="grid grid-cols-6 col-span-1">
+                                    <div class="col-start-1 col-span-1">
+                                        <div class="circulo">
+                                            <p className="inicial">A</p>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="col-start-2 col-end-5">
-                                    <Link href="">Clean Code</Link>
-                                </div>
-                                <div class="col-start-6 col-span-1">
-                                    <div class="imagenLibro">
-                                        <Image src="https://images.cdn2.buscalibre.com/fit-in/180x180/1f/a6/1fa666e0f93fb0bc63c4c214188f3a46.jpg" width={80} height={101}></Image>
+                                    <div class="col-start-2 col-end-5">
+                                        <div className="contenedorTituloLibro">
+                                            <div class="line-clamp-2">
+                                                <p class= "tituloLibro"><b>"Software la Superbabosa"</b></p>
+                                            </div>
+                                        </div>
+                                        <div className="contenedorInfoLibro">
+                                            <div class="line-clamp-1">
+                                                <p className="infoLibro">Cantidad restante: 2</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-start-6 col-span-1">
+                                        <div class="imagenLibro">
+                                            <Image src="https://images.cdn2.buscalibre.com/fit-in/180x180/1f/a6/1fa666e0f93fb0bc63c4c214188f3a46.jpg" width={80} height={101}></Image>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </Link>    
                     </div>
                     <div class="col-start-4 col-span-1">
-                        <div class="libro">
-                            <div class="grid grid-cols-6 col-span-1">
-                                <div class="col-start-1 col-span-1">
-                                    <div className="circulo">
-                                        <p className="inicial">A</p>
+                    <Link href="https://www.buscalibre.pe/libro-software-la-superbabosa/9788423687572/p/2858249">
+                            <div class="libro">
+                                <div class="grid grid-cols-6 col-span-1">
+                                    <div class="col-start-1 col-span-1">
+                                        <div class="circulo">
+                                            <p className="inicial">A</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-start-2 col-end-5">
+                                        <div className="contenedorTituloLibro">
+                                            <div class="line-clamp-2">
+                                                <p class= "tituloLibro"><b>"Gestion de proyectos de Software"</b></p>
+                                            </div>
+                                        </div>
+                                        <div className="contenedorInfoLibro">
+                                            <div class="line-clamp-1">
+                                                <p className="infoLibro">Cantidad restante: 3</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-start-6 col-span-1">
+                                        <div class="imagenLibro">
+                                            <Image src="https://images.cdn1.buscalibre.com/fit-in/180x180/20/82/2082a4e5a59dcf7e8531feb8e488c30b.jpg" width={80} height={101}></Image>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="col-start-2 col-end-5">
-                                    <Link href="/libro/">Introduction to the theory...</Link>
-                                </div>
-                                <div class="col-start-5 col-span-1">
-
-                                </div>
                             </div>
-                        </div> 
+                        </Link>    
                     </div>  
                 </div>
             </div>
