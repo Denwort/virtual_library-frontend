99ee9d33ff3cd539019fe5f872d6d99ae4e482e7
Renzoar500 <renzoar@hotmail.com>
2023-10-07 20:50:53 -0500

    Escribir proximos a vencer

--- src/pages/api/proximos/escribir.js
+++ src/pages/api/proximos/escribir.js
@@ -20,11 +20,25 @@ export default async function leer(req, res) {
         console.log("Ocurrio un error al leer ")
     }
     //Modificar data
-
-    let res
+    let resultado=[]
+    const tmp = JSON.stringify(req.body).replace("'",'"')
+    const body = JSON.parse(tmp)
+    data.forEach(element => {
+        if (element.cuenta.id == body.id) {
+            let aux = {}
+            aux['id'] = element.libro.id
+            aux['titulo'] = element.libro.titulo
+            aux['imagen'] = element.libro.imagen
+            aux['fecha_final'] = element.fecha_inicio
+            resultado.push(aux)
+        }
+    });
+    resultado.sort((a,b) => new Date(a.fecha_final).getTime() - new Date(b.fecha_final).getTime())
+    console.log(resultado)
+        
     // Escritura
     try {
-        let tmp = JSON.stringify(data,null,'\t')
+        let tmp = JSON.stringify(resultado,null,'\t')
         console.log( tmp )
 
         await fsPromises.writeFile( ruta, tmp )
@@ -33,5 +47,4 @@ export default async function leer(req, res) {
     } catch( error) {
         console.log("Ocurrio un error al Escribir ", {error})
     }
-
 }
